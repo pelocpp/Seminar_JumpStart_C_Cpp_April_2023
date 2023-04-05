9ce1c24d6b2f7514eb25a24abf4e1f195b52ab22
Peter Loos <peter.loos@gmx.de>
2023-04-05 16:56:59 +0200

    Wednesday - 17.00.

--- FirstSteps/Strukturen.cpp
+++ FirstSteps/Strukturen.cpp
@@ -43,6 +43,51 @@ void increment (Time* time)
     time->second++;
 }
 
+void incrementEx(Time* time)
+{
+    time->second++;
+
+    if (time->second >= 60)
+    {
+        time->second = 0;
+        time->minute++;
+
+        if (time->minute >= 60)
+        {
+            time->minute = 0;
+            time->hour++;
+
+            if (time->hour >= 24)
+            {
+                time->hour = 0;
+            }
+        }
+    }
+}
+
+bool lessThan(Time* t1, Time* t2)
+{
+    if (t1->hour < t2->hour) {
+        return true;
+    }
+    else if (t1->hour == t2->hour && t1->minute < t2->minute) {
+        return true;
+    }
+    else if (t1->hour == t2->hour && t1->minute == t2->minute && t1->second < t2->second)
+    {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Amount of seconds of a Time variable
+int timeToSeconds(Time* t)
+{
+    return t->hour * 3600 + t->minute * 60 + t->second;
+}
+
 
 void main_structs_01()
 {
