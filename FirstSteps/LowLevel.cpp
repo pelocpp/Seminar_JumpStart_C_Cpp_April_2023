ab2e61c746f38b4fabe72cf3c60b539c6a4f6293
Peter Loos <peter.loos@gmx.de>
2023-04-05 12:02:29 +0200

    Wednesday Noon

--- FirstSteps/LowLevel.cpp
+++ FirstSteps/LowLevel.cpp
@@ -73,7 +73,76 @@ void main_low_level_04()
     std::cout << bs2 << std::endl;
 }
 
+void main_low_level_05()
+{
+    unsigned char byte = 0b0010'0011;
+    std::bitset<8> bs(byte);
+
+    unsigned char byteShifted = byte << 1;
+    std::bitset<8> bsShifted(byteShifted);
+
+    std::cout << byte << std::endl;
+    std::cout << byteShifted << std::endl;
+
+    std::cout << bs << std::endl;
+    std::cout << bsShifted << std::endl;
+}
+
+void main_low_level_06_unsigned()
+{
+    unsigned char byte = 0b1010'0011;
+    std::bitset<8> bs(byte);
+
+    unsigned char byteShifted = byte >> 1;
+    std::bitset<8> bsShifted(byteShifted);
+
+    std::cout << byte << std::endl;
+    std::cout << byteShifted << std::endl;
+
+    std::cout << bs << std::endl;
+    std::cout << bsShifted << std::endl;
+}
+
+void main_low_level_07_signed()
+{
+    signed char byte = 0b1010'0011;
+    std::bitset<8> bs(byte);
+
+    signed char byteShifted = byte >> 1;
+    std::bitset<8> bsShifted(byteShifted);
+
+    std::cout << byte << std::endl;
+    std::cout << byteShifted << std::endl;
+
+    std::cout << bs << std::endl;
+    std::cout << bsShifted << std::endl;
+}
+
+// ===========================================
+
+void setBit(unsigned short value, int pos)
+{
+    unsigned short mask = 1 << pos;
+
+    value = value | mask;
+}
+
+void setBitEx(unsigned short* value, int pos)
+{
+    unsigned short mask = 1 << pos;
+
+    *value = *value | mask;
+}
+
 void main_low_level()
 {
-    main_low_level_04();
+    unsigned short s = 0b1011'0011;
+    std::bitset<16> bsBefore(s);
+    std::cout << bsBefore << std::endl;
+
+    setBitEx(& s, 3);
+
+    std::bitset<16> bsAfter(s);
+    std::cout << bsAfter << std::endl;
+
 }
