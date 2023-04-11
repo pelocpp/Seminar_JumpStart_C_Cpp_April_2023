0d45767c0ebce57bf2951b0fccf26605ef61197f
Peter Loos <peter.loos@gmx.de>
2023-04-11 13:59:26 +0200

    Exercise Account

--- FirstSteps/Inheritance.cpp
+++ FirstSteps/Inheritance.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
-class Rectangle
+// Concept / Interface
+class IRectangleConcept
+{
+public:
+    virtual void paint() = 0;   // NO IMPLEMENTATION
+    virtual void eraseBackground() = 0;   // NO IMPLEMENTATION
+    virtual void drawBorder() = 0;
+
+private:
+    int m_data;
+};
+
+// ==========================================
+
+class Rectangle : public IRectangleConcept
 {
 public:
     // c'tors
@@ -31,7 +45,11 @@ public:
     virtual void paint() {
         std::cout << "in Class Rectangle" << std::endl;
     }
-};
+
+    // Extremely short implementation
+    void eraseBackground() override {}
+    void drawBorder() override {}
+}; 
 
 class ColoredRectangle : public Rectangle
 {
@@ -53,11 +71,39 @@ private:
 
 public:
     // public interface
-    void paint() {
+    void paint() override {
 
-        Rectangle::paint();
+     //   Rectangle::paint();
 
         std::cout << "in class ColoredRectangle: additional stuff" << std::endl;
+    }
+};
+
+class TransparantRectangle : public Rectangle
+{
+public:
+    // memberwise initialization list
+    TransparantRectangle() :
+        // Rectangle(1, 1, 1, 1), m_color(0)
+        // or: Redirecting to a neighboured constructor
+        // -- Note: C++ 17 or higher
+        TransparantRectangle(1, 1, 1, 1, 0)
+    { }
+
+    TransparantRectangle(int top, int left, int width, int height, int opaque)
+        : Rectangle(top, left, width, height), m_opaque(opaque)
+    { }
+
+private:
+    int m_opaque;
+
+public:
+    // public interface
+    void paint() override {
+
+     //   Rectangle::paint();
+
+        std::cout << "in class TransparantRectangle" << std::endl;
     }
 };
 
@@ -69,7 +115,7 @@ void main_inheritance_01()
     cr.paint ();
 }
 
-void main_inheritance()
+void main_inheritance_02()
 {
     ColoredRectangle cr(10, 10, 20, 10, 99);
 
@@ -77,10 +123,83 @@ void main_inheritance()
 
     rp = &cr;   // is - a Relationship
 
-    rp->paint();   // A) ColoredRectangle.paint
-                   //    because cr points to a ColoredRectangle
-}                  // B) Rectangle.paint
-                   //    because rp is type of Rectangle
+    rp->paint();    // A) ColoredRectangle.paint
+                    //    because cr points to a ColoredRectangle
+                    // B) Rectangle.paint
+                    //    because rp is type of Rectangle
+}
+
+void main_inheritance_03()
+{
+    ColoredRectangle cr1;
+    TransparantRectangle tr1;
+    ColoredRectangle cr2;
+    TransparantRectangle tr2;
+    ColoredRectangle cr3;
+
+    Rectangle* rectangles[5] = { &cr1, &tr1, &cr2, &tr2, &cr3 };
+
+    for (int i = 0; i < 5; ++i)
+    {
+        // Greece:  poly
+        // Body:    Morphus
+        //    Polymorphism
+        rectangles[i]->paint();
+    }
+
+    // WITHOUT Virtual // WITHOUT Polymorphism
+    //for (int i = 0; i < 5; ++i)
+    //{
+    //    if (rectangles[i] isTypeOf ColoredRectangle) {
+    //        rectangles[i]->paintOfColoredRectangle();
+    //    }
+    //    else if (rectangles[i] isTypeOf TransparantRectangle) {
+    //        rectangles[i]->paintOfTransparantRectangle();
+    //    }
+    //    else ...
+
+    //}
+}
+
+void main_inheritance_04()
+{
+    ColoredRectangle cr(10, 10, 20, 10, 99);
+
+    ColoredRectangle* crp = &cr;
+
+    crp->paint();
+}
+
+
+IRectangleConcept* FactoryForColoredRectangle()
+{
+    // ........ hidden implementation
+    ColoredRectangle* crp =
+        new ColoredRectangle(10, 10, 20, 10, 99);
+
+    return crp;
+}
+
+void main_inheritance_05()
+{
+    // ColoredRectangle cr(10, 10, 20, 10, 99);
+
+    // decoupling  // Entkopplung // loose coupling
+
+    IRectangleConcept* crp;  // interface pointer
+
+    crp = FactoryForColoredRectangle();
+    crp->drawBorder();
+    crp->eraseBackground();
+    crp->paint();
+}
+
+void main_inheritance()
+{
+    // main_inheritance_01();
+    //main_inheritance_02();
+    main_inheritance_03();
+}
 
 // ==================================
 
